68f3fc34b7f564963f39d4b307cdba5e1d041fe0
Renuka <renuka@Apples-MacBook-Pro-3.local>
2024-07-18 15:07:47 +0530

    js basic concept

--- play.js
+++ play.js
@@ -1,1 +1,60 @@
-console.log('testing')
+//core syntax
+var nameU='Renuka'
+var hobbie='playing'
+
+function summary(userName,hobbie){
+   return('my name is '+userName+' and my hobbies are '+ hobbie)
+}
+
+// console.log(summary(nameU,hobbie))
+
+//let and const
+//arrow function
+// main use of arrow function is to utilise the this key word
+const addOne=(a)=>a+1
+// console.log(addOne(1))
+
+//Objects and properties
+
+const person ={
+    name:'tharun',
+    age:90,
+    greet(){    ///not able to use arrow function 
+        console.log('hi i am '+ this.name)
+    }
+}
+// person.greet()
+
+//Array array methods
+//spread , rest
+const hobbies =['cooking','trekking']
+const newVal = hobbies.slice() //copies the value
+
+const otherWay = [...hobbies] //spread operator
+// console.log(newVal , otherWay)
+
+//rest operator
+const toArray = (...args)=>{
+    return args
+}
+// console.log(toArray(1,2,3,4))
+
+
+//destructuring
+
+const person1 ={
+    name:'tharun',
+    age:90,
+    greet(){    ///not able to use arrow function 
+        console.log('hi i am '+ this.name) 
+    }
+}
+
+let obj=({name,greet})=>{
+    console.log(name , greet) //need to check
+}
+obj(person1)
+
+
+///async ,promise
+//template literals
